0e941e86c9542d647ea9f20115bb3e61d35e3207
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-11-22 11:37:21 -0800

    Modified Credentials.java to properly initialize passwords as well and throws proper exceptions

--- src/com/nguyenmp/gauchospace/Credentials.java
+++ src/com/nguyenmp/gauchospace/Credentials.java
@@ -9,18 +9,28 @@ public class Credentials {
 	private static String mUsername = null;
 	private static String mPassword = null;
 
+	private static void init() throws IOException {
+		// the username and password should be stored as the first
+		// and second line in credentials.txt
+		FileReader fReader = new FileReader("credentials.txt");
+		BufferedReader reader = new BufferedReader(fReader);
+		mUsername = reader.readLine();
+		mPassword = reader.readLine();
+	}
+
 	public static String Username() throws IOException {
 		// If we already loaded a username before, return that!
-		if (mUsername != null) return mUsername;
+		// Otherwise, read a new one
+		if (mUsername != null) init();
 
-		// Otherwise, read in the username for the first time
-		else {
-			FileReader fReader = new FileReader("credentials.txt");
-			BufferedReader reader = new BufferedReader(fReader);
-			mUsername = reader.readLine();
-			mPassword = reader.readLine();
+		return mUsername;
+	}
 
-			return mUsername;
-		}
+	public static String Password() throws IOException {
+		// If we already loaded a password before, return that!
+		// Otherwise, read a new one
+		if (mPassword != null) init();
+
+		return mPassword;
 	}
 }
