3fd29dd6a541dffb931e75a9de85977fe95f768e
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-10-19 12:11:56 -0700

    Added better error handling for Credentials.java

--- src/test/java/com/nguyenmp/gauchospace/Credentials.java
+++ src/test/java/com/nguyenmp/gauchospace/Credentials.java
@@ -1,5 +1,6 @@
 package com.nguyenmp.gauchospace;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,10 +13,18 @@ public class Credentials {
 	private static void init() throws IOException {
 		// the username and password should be stored as the first
 		// and second line in credentials
-		FileReader fReader = new FileReader("credentials");
+        FileReader fReader;
+        try {
+            fReader = new FileReader("credentials");
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("credentials file has not been defined.  Please see the README.md for help.");
+        }
 		BufferedReader reader = new BufferedReader(fReader);
 		mUsername = reader.readLine();
 		mPassword = reader.readLine();
+
+        if (mUsername == null || mPassword == null)
+            throw new IllegalStateException("Username or password have not been defined in the credentials file.");
 	}
 
 	public static String Username() throws IOException {
